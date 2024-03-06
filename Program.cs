23ee4bd16abe3b80b719c0b75254dedf0522434b
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-03-06 11:28:30 -0300

    import class

--- Program.cs
+++ Program.cs
@@ -1,7 +1,19 @@
-﻿namespace HelloWorld {
-    class Program {
-        public static void Main(String[] args) {
-            Console.WriteLine("Hello Gui");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloWorld.Model; // importando classe HelloModelo de HelloWorld.Model
+
+
+namespace HelloWorld
+{ // namespace é equivalente ao package em java
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            HelloModel model = new(); // ou new HelloModel()
+            Console.WriteLine("Hello " + model.Name);
+            Console.Read(); // espera o user digitar algo para seguir a execução
         }
     }
 }
